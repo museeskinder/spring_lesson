be4f2f75bdcfa1d854974da692f63273f6f6ae03
museeskinder <eskindermuse@gmail.com>
2023-09-11 12:49:21 +0300

    component annotation

---
+++ Stereotype_Annotations/src/main/java/com/example/config/StudentConfig.java
@@ -0,0 +1,9 @@
+package com.example.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.example.beans")
+public class StudentConfig {
+}

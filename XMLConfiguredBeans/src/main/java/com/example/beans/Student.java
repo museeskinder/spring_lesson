1142cbacc978f680d97d48439130e5fa5f9e3b88
museeskinder <eskindermuse@gmail.com>
2023-09-13 12:07:22 +0300

    xml beans

---
+++ XMLConfiguredBeans/src/main/java/com/example/beans/Student.java
@@ -0,0 +1,13 @@
+package com.example.beans;
+
+public class Student {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}

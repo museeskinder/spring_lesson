783024ac3816b63ca6be86474caa207404ebc19a
museeskinder <eskindermuse@gmail.com>
2023-09-22 15:05:41 +0300

    fixed UnsatisfiedDepndencyException

--- Around_advice/src/main/java/com/example/config/ProjectConfig.java
+++ Around_advice/src/main/java/com/example/config/ProjectConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @Configuration
-@ComponentScan(basePackages = {"com.example.service", "com.example.implementations"})
+@ComponentScan(basePackages = {"com.example.service", "com.example.implementations", "com.example.aspects"})
 @EnableAspectJAutoProxy
 public class ProjectConfig {
 }
--- Around_advice/src/main/java/com/example/main/Driver.java
+++ Around_advice/src/main/java/com/example/main/Driver.java
@@ -1,13 +1,15 @@
 package com.example.main;
 
+import com.example.config.ProjectConfig;
 import com.example.service.VehicleService;
 import com.example.util.Song;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Driver {
     public static void main(String[] args) {
-        var context = new AnnotationConfigApplicationContext(VehicleService.class);
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
         VehicleService vehicleService = context.getBean(VehicleService.class);
+        System.out.println(vehicleService.getClass());
         Song song = new Song();
         song.setName("eleven");
         song.setArtist("khalid");

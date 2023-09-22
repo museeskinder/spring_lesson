3221ef440db01be609c800a779853068b40ef5a0
museeskinder <eskindermuse@gmail.com>
2023-09-22 15:08:00 +0300

    fixed UnsatisfiedDependencyException

--- Spring_aspects/src/main/java/com/example/main/Driver.java
+++ Spring_aspects/src/main/java/com/example/main/Driver.java
@@ -1,16 +1,18 @@
 package com.example.main;
 
+import com.example.config.ProjectConfig;
 import com.example.service.VehicleService;
 import com.example.util.Song;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Driver {
     public static void main(String[] args) {
-        var context = new AnnotationConfigApplicationContext(VehicleService.class);
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
         Song song = new Song();
         song.setSongArtist("Khalid");
         song.setSongTitle("eleven");
         VehicleService vehicleService = context.getBean(VehicleService.class);
+        System.out.println(vehicleService.getClass());
         String music = vehicleService.playMusic(true, song);
         String move = vehicleService.moveVehicle(true);
         String stop = vehicleService.applyBrake(true);

627d50d8dad5e028df7fe5d8d1de792ad9c88f85
museeskinder <eskindermuse@gmail.com>
2023-09-23 10:08:34 +0300

    util and interfaces

---
+++ Before_aspect/src/main/java/com/example/util/Song.java
@@ -0,0 +1,22 @@
+package com.example.util;
+
+public class Song {
+    private String songName;
+    private String songArtist;
+
+    public String getSongName() {
+        return songName;
+    }
+
+    public void setSongName(String songName) {
+        this.songName = songName;
+    }
+
+    public String getSongArtist() {
+        return songArtist;
+    }
+
+    public void setSongArtist(String songArtist) {
+        this.songArtist = songArtist;
+    }
+}

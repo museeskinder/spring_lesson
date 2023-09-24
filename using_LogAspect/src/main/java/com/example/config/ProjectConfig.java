b094621c42f6ca301788f3c9ab50e46d85f070e7
museeskinder <eskindermuse@gmail.com>
2023-09-24 20:32:07 +0300

    added project config

---
+++ using_LogAspect/src/main/java/com/example/config/ProjectConfig.java
@@ -0,0 +1,9 @@
+package com.example.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = {"com.example.service", "com.example.implementation", "com.example.aspect"})
+public class ProjectConfig {
+}

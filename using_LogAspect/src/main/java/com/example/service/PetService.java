207828b3a549181819e46119aceab4022fa28e79
museeskinder <eskindermuse@gmail.com>
2023-09-24 20:40:43 +0300

    added service bean

---
+++ using_LogAspect/src/main/java/com/example/service/PetService.java
@@ -0,0 +1,25 @@
+package com.example.service;
+
+import com.example.interfaces.Animal;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PetService {
+    @Autowired
+    private Animal animal;
+
+    public Animal getAnimal() {
+        return animal;
+    }
+
+    public void setAnimal(Animal animal) {
+        this.animal = animal;
+    }
+
+    public PetService() {
+        System.out.println("PetService bean has been created");
+    }
+
+
+}

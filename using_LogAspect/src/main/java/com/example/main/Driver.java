74505d1503216c00346523692c26ef8420edf01f
museeskinder <eskindermuse@gmail.com>
2023-09-25 14:50:57 +0300

    context fetching

--- using_LogAspect/src/main/java/com/example/main/Driver.java
+++ using_LogAspect/src/main/java/com/example/main/Driver.java
@@ -1,7 +1,15 @@
 package com.example.main;
 
+import com.example.config.ProjectConfig;
+import com.example.service.PetService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Driver {
     public static void main(String[] args) {
-
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+        PetService petService = context.getBean(PetService.class);
+        System.out.println(petService.getClass());
+        boolean animalWake = false;
+        String petStatus = petService.feed(animalWake);
     }
 }
